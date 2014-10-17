73ddaf7f3ebba8e65457ae80f27fbf972faf72cc
Richard Hitier <richard.hitier@co-libri.org>
2014-10-17 15:36:41 +0200

    Init

---
+++ js/main.js
@@ -0,0 +1,57 @@
+var z = 7;
+var myLL = L.latLng(46.725,1.785);
+
+
+// create a map in the "map" div, set the view to a given place and zoom
+var map = L.map('map', {
+    center: myLL,
+    zoom: z
+});
+
+
+// add an OpenStreetMap tile layer
+var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: 'OpenStreetMap'
+}).addTo(map);
+
+var esriLayer = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+var mapqLayer = L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+    attribution: 'MapQuest OpenStreetMap',
+    subdomains: ['otile1', 'otile2', 'otile3', 'otile4']
+});
+
+// var offices_layer = 1;
+
+
+
+
+
+var baseLayers = {
+    "Satellite": esriLayer,
+    "OSM": osmLayer,
+    "MapBox": mapqLayer,
+};
+//
+//var overLays = {
+//    "Offices Du Tourisme": offices_layer,
+//};
+
+// Layers switchers
+L.control.layers(baseLayers, overLays).setPosition('topright').addTo(map);
+
+// Scale at bottom left
+L.control.scale().addTo(map);
+
+// Rewrite url to show lat/lon/zoom
+// (uses leaflet-hash plugin as submodule)
+var hash = new L.Hash(map);
+
+// search field to find place
+// (use leaflet-geocoding plugin as submodule)
+new L.Control.GeoSearch({
+    provider: new L.GeoSearch.Provider.OpenStreetMap(),
+    zoomLevel: 15,
+}).addTo(map);
